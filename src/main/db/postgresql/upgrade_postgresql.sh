d5ae01f6a79687e163d5c91b1d67a5b153cfcf37
dslaveykov <dslaveykov@axway.com>
2019-09-04 13:27:30 +0300

    [PSQL] Abort upgrade if PGPASSWORD env variable is not found/set

--- src/main/db/postgresql/upgrade_postgresql.sh
+++ src/main/db/postgresql/upgrade_postgresql.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 OLD_DB_VERSION=4.0.5
-NEW_DB_VERSION="4.0.6"
+NEW_DB_VERSION=4.0.6
 NEEDS_UPGRADE=false
 
 # save the starting folder location
@@ -33,15 +33,20 @@ then
 	export PGPASSWORD="$SECOND_CMD_ARGUMENT"
 fi
 
+if [ -z "$PGPASSWORD" ];
+then
+    echo "PGPASSWORD env variable not found/set. Aborting upgrade"
+   exit 1
+fi
+
 DATABASE_EXISTS=0
-until [ "$DATABASE_EXISTS" == 1 ]; do		
+until [ "$DATABASE_EXISTS" == 1 ]; do
 
 	if [ -z "$FIRST_CMD_ARGUMENT" ]; then
 		read -p 'Enter Database name: ' DB_NAME
 	else
 		DB_NAME="$FIRST_CMD_ARGUMENT"
 	fi
-	
 	# see if database exists
 	# DATABASE_EXISTS=`psql -U AtsUser -h localhost -l | grep $DB_NAME | wc -l`
 	DATABASE_EXISTS=`psql -U postgres -h localhost -l | grep $DB_NAME | wc -l`
@@ -51,11 +56,11 @@ until [ "$DATABASE_EXISTS" == 1 ]; do
 		if [ ! -z "$FIRST_CMD_ARGUMENT" ];
 		then
 			echo "No database exist with the given name. Now will exit"
-			exit 1
+			exit 2
 		else
 			echo "No database exist with the given name. Plese select another name"
-		fi	
-	fi	
+		fi
+	fi
 done
 
 # get database version and change NEEDS_UPGRADE flag if needed
@@ -75,7 +80,7 @@ if [ "$NEEDS_UPGRADE" = true ]; then
 	echo "UPDATE \"tInternal\" SET value = '${NEW_DB_VERSION}_draft' WHERE key = 'version';" >> tmpUpgradeDbScript.sql
 	echo " " >> tmpUpgradeDbScript.sql
 	cat TestExplorerDb_PostgreSQL_Upgrade.sql >> tmpUpgradeDbScript.sql
-	psql -U postgres -a -f tmpUpgradeDbScript.sql | grep 'ERROR:\|WARNING:' > upgrade.log
+	psql -U postgres -h localhost -a -f tmpUpgradeDbScript.sql | grep 'ERROR:\|WARNING:' > upgrade.log
 	NUM_OF_ERRORS=`cat upgrade.log | grep 'ERROR:' | wc -l`
 	if [[ "$NUM_OF_ERRORS" == 0 ]]; then
 		psql -U postgres -h localhost -d $DB_NAME -t -c "UPDATE \"tInternal\" SET value = '$NEW_DB_VERSION' WHERE key = 'version'"
@@ -83,16 +88,15 @@ if [ "$NEEDS_UPGRADE" = true ]; then
 		echo "Errors during install: $NUM_OF_ERRORS. See upgrade.log file for errors"
 		if [ ! -z "$FIRST_CMD_ARGUMENT" ];
 		then
-			exit 2
+			exit 3
 		fi
 	fi
 
 	echo "Installing of \"$DB_NAME\" completed. See install.log file for potential errors"
 	if [ ! -z "$FIRST_CMD_ARGUMENT" ];
-		then
-			exit 0
-		fi
-	
+	then
+	    exit 0
+	fi
 	# back to the starting folder location
 	cd $START_FOLDER
 
@@ -100,7 +104,6 @@ else
 	echo "Could not upgrade \"$DB_NAME\" from \"$DB_VERSION\" to \"$NEW_DB_VERSION\""
 	if [ ! -z "$FIRST_CMD_ARGUMENT" ];
 	then
-		exit 3
-	fi	
+		exit 4
+	fi
 fi
-
