554d09f7cd0d51e364ed799af4aa698dd4b755ab
dslaveykov <dslaveykov@axway.com>
2019-09-04 13:25:29 +0300

    Revert "Skip upgrade if env var PGPASSWORD is not found/set"
    This reverts commit 4f278d88b48e911d1edc3b438f9335d9ce917f1e.

--- src/main/db/postgresql/install_postgresql.sh
+++ src/main/db/postgresql/install_postgresql.sh
@@ -1,109 +1,84 @@
 #!/usr/bin/env bash
 
-OLD_DB_VERSION=4.0.5
-NEW_DB_VERSION=4.0.6
-NEEDS_UPGRADE=false
-
 # save the starting folder location
 START_FOLDER="$PWD"
 
-# navigate to the upgrade file directory
+# navigate to the install file directory
 cd `dirname $0`
 
+# delete previous tmpInstallDbScript.sql if one exists
+rm -rf tmpInstallDbScript.sql
+touch tmpInstallDbScript.sql
 
-# delete previous tmpUpgradeDbScript.sql if one exists
-rm -rf tmpUpgradeDbScript.sql
-touch tmpUpgradeDbScript.sql
+# delete previous install.log if one exists
+rm -rf install.log
+touch install.log
 
-# delete previous upgrade.log if one exists
-rm -rf upgrade.log
-touch upgrade.log
+CMD_ARGUMENT="$1"
 
-FIRST_CMD_ARGUMENT="$1"
-SECOND_CMD_ARGUMENT="$2"
-
-if [[ "$FIRST_CMD_ARGUMENT" == "/?" || "$FIRST_CMD_ARGUMENT" == "--help" ]] ; then
-	echo "Please specify the database name as first parameter and database password as second parameter for silent upgrade"
+if [[ "$CMD_ARGUMENT" == "/?" || "$CMD_ARGUMENT" == "--help" ]] ; then
+	echo "Please specify the database name as first parameter and database password as second parameter for silent install"
 	exit
 fi
 
 # change password if needed
-if [ ! -z "$SECOND_CMD_ARGUMENT" ];
+if [ ! -z "$2" ];
 then
-	export PGPASSWORD="$SECOND_CMD_ARGUMENT"
+	export PGPASSWORD="$2"
 fi
 
-if [ -z "$PGPASSWORD" ];
-then
-    echo "PGPASSWORD env variable not found/set. Aborting upgrade"
-   exit 1
-fi
+DATABASE_EXISTS=1
+until [ "$DATABASE_EXISTS" == 0 ]; do		
 
-DATABASE_EXISTS=0
-until [ "$DATABASE_EXISTS" == 1 ]; do
-
-	if [ -z "$FIRST_CMD_ARGUMENT" ]; then
+	if [ -z "$CMD_ARGUMENT" ];
+	then
 		read -p 'Enter Database name: ' DB_NAME
 	else
-		DB_NAME="$FIRST_CMD_ARGUMENT"
+		DB_NAME="$CMD_ARGUMENT"
 	fi
+		
 	# see if database exists
-	# DATABASE_EXISTS=`psql -U AtsUser -h localhost -l | grep $DB_NAME | wc -l`
+	# psql -U postgres -h localhost -l | grep $DB_NAME | wc -l`
 	DATABASE_EXISTS=`psql -U postgres -h localhost -l | grep $DB_NAME | wc -l`
-
-	if [ "$DATABASE_EXISTS" == 0 ];
+	
+	if [ "$DATABASE_EXISTS" != 0 ];
 	then
-		if [ ! -z "$FIRST_CMD_ARGUMENT" ];
+		if [ ! -z "$CMD_ARGUMENT" ];
 		then
-			echo "No database exist with the given name. Now will exit"
-			exit 2
+			echo Database name "$CMD_ARGUMENT" already exist. Install abort
+			exit 1
 		else
-			echo "No database exist with the given name. Plese select another name"
+			echo Database name "$CMD_ARGUMENT" already exist. Please choose another name
 		fi
 	fi
 done
+		
 
-# get database version and change NEEDS_UPGRADE flag if needed
-# DB_VERSION=`psql -U AtsUser -h localhost -d $DB_NAME -t -c "SELECT \"value\" FROM \"tInternal\" WHERE \"key\" = 'version'" | xargs` # | xargs is used to trim the db version string
-DB_VERSION=`psql -U postgres -h localhost -d $DB_NAME -t -c "SELECT \"value\" FROM \"tInternal\" WHERE \"key\" = 'version'" | xargs` # | xargs is used to trim the db version string
+echo "Installing \"$DB_NAME ..."
+echo "CREATE DATABASE \"$DB_NAME\";" >> tmpInstallDbScript.sql
+echo " " >> tmpInstallDbScript.sql
+echo "\connect $DB_NAME" >> tmpInstallDbScript.sql
+echo " " >> tmpInstallDbScript.sql
+cat TestExplorerDb_PostgreSQL.sql >> tmpInstallDbScript.sql
 
-if [ "$DB_VERSION" = "$OLD_DB_VERSION"  ]; then
-	NEEDS_UPGRADE=true
+psql -U postgres -h localhost -a -f tmpInstallDbScript.sql | grep 'ERROR:' > install.log
+NUM_OF_ERRORS=`cat install.log | grep 'ERROR:' | wc -l`
+
+
+if [[ "$NUM_OF_ERRORS" == 0 ]]; then
+	echo "Installing of \"$DB_NAME\" completed. Logs are located in install.log file"
+	if [ ! -z "$CMD_ARGUMENT" ];
+	then
+		exit 2
+	fi
 else
-	NEEDS_UPGRADE=false
+	echo "Errors during install: $NUM_OF_ERRORS"
+	echo "Installing of \"$DB_NAME\" was not successful. Logs are located in install.log file"
+	if [ ! -z "$CMD_ARGUMENT" ];
+	then
+		exit 0
+	fi
 fi
 
-if [ "$NEEDS_UPGRADE" = true ]; then
-	echo "UPGRADING \"$DB_NAME\" from version \"$DB_VERSION\" to \"$NEW_DB_VERSION\""
-	echo "\connect $DB_NAME" >> tmpUpgradeDbScript.sql
-	echo " " >> tmpUpgradeDbScript.sql
-	echo "UPDATE \"tInternal\" SET value = '${NEW_DB_VERSION}_draft' WHERE key = 'version';" >> tmpUpgradeDbScript.sql
-	echo " " >> tmpUpgradeDbScript.sql
-	cat TestExplorerDb_PostgreSQL_Upgrade.sql >> tmpUpgradeDbScript.sql
-	psql -U postgres -h localhost -a -f tmpUpgradeDbScript.sql | grep 'ERROR:\|WARNING:' > upgrade.log
-	NUM_OF_ERRORS=`cat upgrade.log | grep 'ERROR:' | wc -l`
-	if [[ "$NUM_OF_ERRORS" == 0 ]]; then
-		psql -U postgres -h localhost -d $DB_NAME -t -c "UPDATE \"tInternal\" SET value = '$NEW_DB_VERSION' WHERE key = 'version'"
-	else
-		echo "Errors during install: $NUM_OF_ERRORS. See upgrade.log file for errors"
-		if [ ! -z "$FIRST_CMD_ARGUMENT" ];
-		then
-			exit 3
-		fi
-	fi
-
-	echo "Installing of \"$DB_NAME\" completed. See install.log file for potential errors"
-	if [ ! -z "$FIRST_CMD_ARGUMENT" ];
-	then
-	    exit 0
-	fi
-	# back to the starting folder location
-	cd $START_FOLDER
-
-else
-	echo "Could not upgrade \"$DB_NAME\" from \"$DB_VERSION\" to \"$NEW_DB_VERSION\""
-	if [ ! -z "$FIRST_CMD_ARGUMENT" ];
-	then
-		exit 4
-	fi
-fi
+# back to the starting folder location
+cd $START_FOLDER
