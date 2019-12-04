edf038a36fa11a45f2829afd4426effe65cbca10
Dragoslav Slaveykov <dslaveykov@axway.com>
2019-12-04 11:07:20 +0200

    Update TestExplorerDb_PostgreSQL_Upgrade.sql

--- src/main/db/postgresql/TestExplorerDb_PostgreSQL_Upgrade.sql
+++ src/main/db/postgresql/TestExplorerDb_PostgreSQL_Upgrade.sql
@@ -3,3 +3,31 @@
 -- ***********************************************
 
 \set ON_ERROR_STOP on
+
+DO language plpgsql $$
+BEGIN
+  RAISE WARNING '#18 INTERNAL VERSION UPGRADE HEADER - START';
+END
+$$;
+INSERT INTO "tInternal" (key, value) VALUES ('Upgrade_to_intVer_18', NOW());
+DO language plpgsql $$
+BEGIN
+  RAISE WARNING '#18 INTERNAL VERSION UPGRADE HEADER - END';
+END
+$$;
+
+-- Add changes for 4.0.7 (next release) here
+--
+--
+
+DO language plpgsql $$
+BEGIN
+  RAISE WARNING '#18 INTERNAL VERSION UPGRADE FOOTER - START';
+END
+$$;
+UPDATE "tInternal" SET value='18' WHERE key='internalVersion';
+DO language plpgsql $$
+BEGIN
+  RAISE WARNING '#18 INTERNAL VERSION UPGRADE FOOTER - END';
+END
+$$;
