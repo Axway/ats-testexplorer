147fc31e88a733afcfab1b2b61e8fa118261032d
svilen-d <svilen-d@users.noreply.github.com>
2020-12-31 12:30:57 +0200

    [Db scripts] PostgreSQL, Win script - describe env. variables for connection using non-default values

--- src/main/db/postgresql/install_postgresql.cmd
+++ src/main/db/postgresql/install_postgresql.cmd
@@ -1,4 +1,23 @@
 @echo off
+rem Environment variables to use if non-default DB environment is used. Below are listed default values
+rem set PGHOST=localhost
+rem set PGPORT=5432
+rem set PGUSER=postgres - 'postgres' admin DB user is used to create the new DB and create regular DB user
+
+rem set host to connect to
+IF [%PGHOST%]==[] (
+    set PGHOST=localhost
+) ELSE (
+    echo PGHOST environment variable is defined with value: %PGHOST%
+)
+
+rem set port to connect to
+IF [%PGPORT%]==[] (
+    set PGPORT=5432
+) ELSE (
+    echo PGPORT environment variable is defined with value: %PGPORT%
+)
+
 
 :: save the starting folder location
 set START_FOLDER=%cd%
@@ -22,14 +41,15 @@ set HELP=false
 IF [%FIRST_CMD_ARGUMENT%]==[--help] set HELP=true
 IF [%FIRST_CMD_ARGUMENT%]==[/?] set HELP=true
 IF "%HELP%" == "true" (
-    echo Please specify the database name as first parameter and database password as second parameter for silent install
+    echo Please specify the database name as first parameter and postgres user's password as second parameter for silent install
+    echo Example: install_postgres.cmd MyDB postgresPwd
     GOTO :end
 )
 
 :: check if the script is executed manually
 set INTERACTIVE=0
 echo %cmdcmdline% | find /i "%~0" >nul
-if not errorlevel 1 set INTERACTIVE=1
+IF NOT ERRORLEVEL 1 set INTERACTIVE=1
 
 IF %INTERACTIVE% == 0 (
 	SET CONSOLE_MODE_USED=true
@@ -40,7 +60,7 @@ IF %INTERACTIVE% == 0 (
 		SET SILENT_MODE_USED=true
 	)
 )
-
+echo "Silent mode used: %SILENT_MODE_USED%"
 :set_dbname
 IF "%SILENT_MODE_USED%" == "true" (
 	set DB_NAME=%FIRST_CMD_ARGUMENT%
@@ -59,7 +79,7 @@ IF NOT [%SECOND_CMD_ARGUMENT%]==[] (
 psql -U postgres -l > db_list.txt
 IF %ERRORLEVEL% NEQ 0 (
 	echo There was problem checking for database existence
-	echo Check if the provided password is correct
+	echo Check if the provided postgres password, host and port are correct. In non-local mode pg_hba.conf should allow connect from current host.
 	IF "%SILENT_MODE_USED%" == "true" (
 		del /f /q db_list.txt
 		exit 1
