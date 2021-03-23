5f752fd70092770a1ef2fe673ff7667a0d71a7bb
svilen-d <svilen-d@users.noreply.github.com>
2021-03-23 11:26:15 +0200

    [DB scripts] Postgre install script fixes - Fix checking tInternal table after install - Add more detailed cause on first DB check (check database existence) - Logging clarifications

--- src/main/db/postgresql/install_postgresql.sh
+++ src/main/db/postgresql/install_postgresql.sh
@@ -1,32 +1,37 @@
 #!/usr/bin/env bash
 
 function print_help() {
-  echo "The usage is ./install.sh [OPTION]...[VALUE]...
+  echo "The usage is ${0} [OPTION] [VALUE] ...
 The following script installs a ATS Logging DB to store test execution results. The version is 4.0.7"
   echo "Available options
--H <target_SQL_server_host>, default is: localhost,Might be specified by env variable: PGHOST
--p <target_SQL_server_port>, default is: 1433, Might be specified by env variable: PGPORT
--d <target_SQL_database_name>, Might be specified by env variable: PGDATABASE
--u <target_SQL_user_name>, default is: AtsUser,Might be specified by env variable: PSQL_USER_NAME
--s <target_SQL_user_password>, default is: AtsPassword,Might be specified by env variable: PSQL_USER_PASSWORD
--U <target_SQL_admin_name>,use current OS account Might be specified by env variable: PGUSER
--S <target_SQL_admin_password>, use current OS account  Might be specified by env variable: PGPASSWORD"
+  -H <target_SQL_server_host>, default is: localhost; Might be specified by env variable: PGHOST
+  -p <target_SQL_server_port>, default is: 1433; Might be specified by env variable: PGPORT
+  -d <target_SQL_database_name>; Might be specified by env variable: PGDATABASE
+  -u <target_SQL_user_name>, default is: AtsUser; Might be specified by env variable: PSQL_USER_NAME
+  -s <target_SQL_user_password>, default is: AtsPassword; Might be specified by env variable: PSQL_USER_PASSWORD
+  -U <target_SQL_admin_name>, default: use current OS account;  Might be specified by env variable: PGUSER
+  -S <target_SQL_admin_password>, default: no; Might be specified by env variable: PGPASSWORD"
 }
 
-function check_db_existance() {
+function check_db_existence() {
 
   PGDATABASE="$1"
   # see if database exists
   # psql -U postgres -h localhost -l | grep $PGDATABASE | wc -l`
 
-  DATABASE_EXISTS=$(psql -h $PGHOST -p $PGPORT -U $PGUSER -l | grep $PGDATABASE | wc -l)
+  DBS_OUTPUT=$(psql -h $PGHOST -p $PGPORT -U $PGUSER -l)
+  if [ $? != 0 ]; then
+      echo "List of installed databases could not be retrieved. Possible cause is wrong host or port parameter, DB admin user or password"
+      exit 6
+  fi
+  DATABASE_EXISTS=$(echo "$DBS_OUTPUT" | grep "$PGDATABASE" | wc -l)
 
   if [ "$DATABASE_EXISTS" != 0 ]; then
     if [ $MODE == $BATCH_MODE ]; then
-      echo Database name "$PGDATABASE" already exist. Install abort
+      echo "Database named $PGDATABASE already exist. Installation will abort."
       exit 3
     else
-      echo Database name "$PGDATABASE" already exist. Please choose another name
+      echo "Database named "$PGDATABASE" already exist. Please choose another name."
     fi
   fi
 
@@ -54,26 +59,26 @@ touch install.log
 if [ -z "$PGHOST" ]; then
   PGHOST=localhost
 else
-  echo PGHOST enviroment variable is defined with the value: $PGHOST
+  echo "PGHOST environment variable is defined with the value: $PGHOST"
 fi
 
 if [ -z "$PGPORT" ]; then
   PGPORT=5433
 else
-  echo PGPORT enviroment variable is defined with the value: $PGPORT
+  echo "PGPORT environment variable is defined with the value: $PGPORT"
 fi
 
 if ! [ -z "$PGDATABASE" ]; then
-  echo PGDATABASE enviroment variable is defined with the value: $PGDATABASE
+  echo "PGDATABASE environment variable is defined with the value: $PGDATABASE. Assuming batch (non-interactive) mode"
   MODE=$BATCH_MODE
 fi
 
 if ! [ -z "$PGUSER" ]; then
-  echo PGUSER enviroment variable is defined with the value: $PGUSER
+  echo "PGUSER environment variable is defined with the value: $PGUSER"
 fi
 
 if ! [ -z "$PGPASSWORD" ]; then
-  echo PGPASSWORD enviroment variable is defined with the value: $PGPASSWORD
+  echo "Using defined PGPASSWORD environment variable"
 fi
 
 export PGPASSWORD=$PGPASSWORD
@@ -81,13 +86,13 @@ export PGPASSWORD=$PGPASSWORD
 if [ -z "$PSQL_USER_NAME" ]; then
   PSQL_USER_NAME=AtsUser
 else
-  echo PSQL_USER_NAME enviroment variable is defined with the value: $PSQL_USER_NAME
+  echo "PSQL_USER_NAME environment variable is defined with the value: $PSQL_USER_NAME"
 fi
 
 if [ -z "$PSQL_USER_PASSWORD" ]; then
   PSQL_USER_PASSWORD=AtsPassword
 else
-  echo PSQL_USER_PASSWORD enviroment variable is defined with the value: $PSQL_USER_PASSWORD
+  echo "PSQL_USER_PASSWORD environment variable is defined with the value: $PSQL_USER_PASSWORD"
 fi
 
 while getopts ":H:p:d:u:s:U:S:h" option; do
@@ -129,7 +134,7 @@ while getopts ":H:p:d:u:s:U:S:h" option; do
     exit 0
     ;;
   \?)
-    echo "Invallid option: -$OPTARG"
+    echo "Invalid option: -$OPTARG"
     print_help
 
     exit 1
@@ -138,27 +143,26 @@ while getopts ":H:p:d:u:s:U:S:h" option; do
 done
 
 if [[ -z "$PGUSER" ]]; then
-  echo "Admin user credentials need to be provided in order to create new database"
-
-  exit 1
+    echo "Admin user credentials need to be provided in order to create new database"
+    exit 1
 fi
 
 until [ "$DATABASE_EXISTS" == 0 ]; do
 
-  if [ $MODE == $INTERACTIVE_MODE ]; then
-    read -p 'Enter Database name: ' PGDATABASE
-  fi
+    if [ $MODE == $INTERACTIVE_MODE ]; then
+        read -p 'Enter Database name: ' PGDATABASE
+    fi
 
-  # see if database exists
-  # psql -U postgres -h localhost -l | grep $PGDATABASE | wc -l`
-  check_db_existance "$PGDATABASE"
-  DATABASE_EXISTS=$?
+    # see if database exists
+    # psql -U postgres -h localhost -l | grep $PGDATABASE | wc -l`
+    check_db_existence "$PGDATABASE"
+    DATABASE_EXISTS=$?
 done
 
 echo "Installing \"$PGDATABASE ..."
 echo "CREATE DATABASE \"$PGDATABASE\";" >>tmpInstallDbScript.sql
 echo " " >>tmpInstallDbScript.sql
-echo "\connect $PGDATABASE" >>tmpInstallDbScript.sql
+echo "\connect \"$PGDATABASE\"" >>tmpInstallDbScript.sql
 echo " " >>tmpInstallDbScript.sql
 cat TestExplorerDb_PostgreSQL.sql >>tmpInstallDbScript.sql
 
@@ -169,16 +173,16 @@ psql -h $PGHOST -p $PGPORT -U $PGUSER -a -f tmpInstallDbScript.sql | grep 'ERROR
 NUM_OF_ERRORS=$(cat install.log | grep 'ERROR:' | wc -l)
 
 export PGDATABASE=$db
-psql -U $PSQL_USER_NAME -h $PGHOST -p $PGPORT password=$PSQL_USER_PASSWORD -c 'SELECT * FROM "tInternal";'
+PGPASSWORD="$PSQL_USER_PASSWORD" psql -U $PSQL_USER_NAME -h "$PGHOST" -p $PGPORT  -c 'SELECT * FROM "tInternal";' # current_database();
 
 if [[ "$NUM_OF_ERRORS" == 0 ]]; then
-  echo "Installing of \"$PGDATABASE\" completed. Logs are located in install.log file"
+  echo "Installation of \"$PGDATABASE\" database completed. Logs are located in install.log file"
   if [ $MODE == $BATCH_MODE ]; then
     exit 2
   fi
 else
-  echo "Errors during install: $NUM_OF_ERRORS"
-  echo "Installing of \"PGDATABASE\" was not successful. Logs are located in install.log file"
+  echo "Errors found during install: $NUM_OF_ERRORS"
+  echo "Installation of \"$PGDATABASE\" database was NOT successful. Logs are located in install.log file"
   if [ $MODE == $BATCH_MODE ]; then
     exit 0
   fi
